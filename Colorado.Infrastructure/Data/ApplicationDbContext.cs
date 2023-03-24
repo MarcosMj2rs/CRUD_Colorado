1209027463200e29429838149aea14f827ad3d36
MarcosMj2rs <marcos.mj2rs@gmail.com>
2023-03-24 09:05:10 -0300

    Ajustes

--- Colorado.Infrastructure/Data/ApplicationDbContext.cs
+++ Colorado.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,6 +6,6 @@ namespace Colorado.Infrastructure.Data
 	public class ApplicationDbContext : DbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-		public DbSet<Cliente> Clientes { get; set; }
+		public DbSet<Cliente> Cliente { get; set; }
 	}
 }
