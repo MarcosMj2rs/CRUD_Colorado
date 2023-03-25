397f4ec44b1da08f6152c47fb41c3f906b04c5f6
MarcosMj2rs <marcos.mj2rs@gmail.com>
2023-03-25 02:33:36 -0300

    Ajuste visualização

---
+++ Colorado.WebApiConsumer.MVC/Controller/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using Colorado.WebApiConsumer.MVC.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Colorado.WebApiConsumer.MVC.Controllers
+{
+    public class ClientesController : ControllerBase
+    {
+        private readonly string apiUrl = " https://localhost:5001/api/Cliente/GetAllAsync";
+
+        public async Task<IActionResult> Index()
+        {
+            List<Cliente> listaClientes = new List<Cliente>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    listaClientes = JsonConvert.DeserializeObject<List<Cliente>>(apiResponse);
+                }
+            }
+            return Ok(listaClientes);
+        }
+    }
+}
