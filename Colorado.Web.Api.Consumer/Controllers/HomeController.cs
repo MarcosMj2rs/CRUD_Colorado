7eb6dbf74049df91b0f15a7b4e05fb739c26bea4
MarcosMj2rs <marcos.mj2rs@gmail.com>
2023-03-26 19:53:48 -0300

    Implementação do Create

--- Colorado.Web.Api.Consumer/Controllers/HomeController.cs
+++ Colorado.Web.Api.Consumer/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using Colorado.Web.Api.Consumer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,21 +15,22 @@ namespace Colorado.Web.Api.Consumer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly string apiUrl = " https://localhost:5001/api/Cliente/GetAllAsync";
+        private readonly string apiUrlHome = "https://localhost:5001/api/Cliente/GetAllAsync";
+        private readonly string apiUrlCreate = "https://localhost:5001/api/Cliente/AddAsync";
+        private readonly string apiUrlGetById = "https://localhost:5001/api/Cliente/GetByIdAsync";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        // GET api/values
         public async Task<IActionResult> Index()
         {
             IEnumerable<ClienteModel> listaClientes = new List<ClienteModel>();
 
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync(apiUrl))
+                using (var response = await httpClient.GetAsync(apiUrlHome))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
                     listaClientes = JsonConvert.DeserializeObject<IEnumerable<ClienteModel>>(apiResponse);
@@ -35,6 +38,53 @@ namespace Colorado.Web.Api.Consumer.Controllers
             }
             return View(listaClientes);
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ClienteModel cliente)
+        {
+            if (cliente == null)
+                return View();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(apiUrlCreate);
+                var postTask = await httpClient.PostAsJsonAsync<ClienteModel>(httpClient.BaseAddress.AbsoluteUri, cliente);
+
+                if (postTask.IsSuccessStatusCode)
+                    return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Erro no Processar página.");
+            return View(cliente);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(ClienteModel cliente)
+        {
+            if (cliente.Id == 0)
+                return BadRequest();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(apiUrlGetById);
+                var postTask = await httpClient.PostAsJsonAsync<ClienteModel>(httpClient.BaseAddress.AbsoluteUri, cliente);
+
+                if (postTask.IsSuccessStatusCode)
+                    return View(cliente);
+            }
+
+            ModelState.AddModelError(string.Empty, "Erro no Processar página.");
+            return RedirectToAction("edit");
+        }
+
+
+
 
         public IActionResult Privacy()
         {
--- Colorado.Web.Api.RestFull/Controllers/ClienteController.cs
+++ Colorado.Web.Api.RestFull/Controllers/ClienteController.cs
@@ -1,6 +1,8 @@
 ﻿using Colorado.Core.Entities;
 using Colorado.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Colorado.Web.Api.Controller
@@ -20,27 +22,42 @@ namespace Colorado.Web.Api.Controller
 
 		[HttpPost]
 		[Route("GetByIdAsync")]
-		public async Task<IActionResult> GetByIdAsync(int id)
+		public async Task<IActionResult> GetByIdAsync([FromBody] Cliente cliente)
 		{
-			var data = await _cliente.GetByIdAsync(id);
-			return data is null ? null : Ok(data);
+			try
+			{
+				var data = await _cliente.GetByIdAsync(cliente.Id);
+				return Ok(data);
+			}
+			catch (WebException ex) { return BadRequest(ex.Message); }
+			catch (Exception ex) { return BadRequest(ex.Message); }
 		}
 
 		[HttpGet]
 		[Route("GetAllAsync")]
 		public async Task<IActionResult> GetAllAsync()
 		{
-			var data = await _cliente.GetAllAsync();
-			return data is null ? null : Ok(data);
+			try
+			{
+				var data = await _cliente.GetAllAsync();
+				return Ok(data);
+			}
+			catch (WebException ex) { return BadRequest(ex.Message); }
+			catch (Exception ex) { return BadRequest(ex.Message); }
 		}
 
 		[HttpPost]
 		[Route("AddAsync")]
 		public async Task<IActionResult> AddAsync([FromBody] Cliente cliente)
 		{
-			var data = await _cliente.AddAsync(cliente);
-			await _unitOfWork.Commit();
-			return data is null ? null : Ok(data.Id);
+			try
+			{
+				var data = await _cliente.AddAsync(cliente);
+				await _unitOfWork.Commit();
+				return Ok(data);
+			}
+			catch (WebException ex) { return BadRequest(ex.Message); }
+			catch (Exception ex) { return BadRequest(ex.Message); }
 		}
 
 		[HttpPost]
@@ -52,7 +69,8 @@ namespace Colorado.Web.Api.Controller
 				await _cliente.UpdateAsync(cliente);
 				await _unitOfWork.Commit();
 			}
-			catch { return null; }
+			catch (WebException ex) { return BadRequest(ex.Message); }
+			catch (Exception ex) { return BadRequest(ex.Message); }
 			return Ok(true);
 		}
 
@@ -65,7 +83,8 @@ namespace Colorado.Web.Api.Controller
 				await _cliente.DeleteAsync(cliente);
 				await _unitOfWork.Commit();
 			}
-			catch { return null; }
+			catch (WebException ex) { return BadRequest(ex.Message); }
+			catch (Exception ex) { return BadRequest(ex.Message); }
 			return Ok(true);
 		}
 	}
