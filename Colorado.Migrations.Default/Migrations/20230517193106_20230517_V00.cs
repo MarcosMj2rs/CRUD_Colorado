d02aee9040eff8140dd38555916a5ea19b8b7edb
MarcosMj2rs <marcos.mj2rs@gmail.com>
2023-05-17 16:51:42 -0300

    Adição de projeto especifico para Migrations, um uma migração

---
+++ Colorado.Migrations.Default/Migrations/20230517193106_20230517_V00.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colorado.Migrations.Default.Migrations
+{
+    public partial class _20230517_V00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodigoCliente = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    Endereco = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(maxLength: 2, nullable: true),
+                    DataCadastro = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
